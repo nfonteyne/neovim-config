08f327625eefe196681857c7e38f0093e6d817d1
Nathan Fonteyne <nathan.fonteyne@axians.com>
2025-06-06 17:30:07 +0200

    change recording display in Noice

--- lua/jfork/plugins/noice.lua
+++ lua/jfork/plugins/noice.lua
@@ -4,7 +4,7 @@ return {
   opts = {
       routes = {
       {
-        view = "notify", -- or another view of your choice
+        view = "cmdline", -- or another view of your choice
         filter = { event = "msg_showmode" },
       },
     }, 
