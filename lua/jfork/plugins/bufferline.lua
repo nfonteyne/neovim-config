c20115ce50803967c89539914fe5d1e1d9bbe30c
FONTEYNE Nathan <nathan.fonteyne@vinci-energies.net>
2025-09-10 16:59:48 +0200

    add keymaps to bufferline and add smartsplits to facilitate navigation on wezterm

--- lua/jfork/plugins/bufferline.lua
+++ lua/jfork/plugins/bufferline.lua
@@ -1,8 +1,15 @@
 return {
   'akinsho/bufferline.nvim',
   version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  dependencies = {'nvim-tree/nvim-web-devicons'},
   config = function()
     require("bufferline").setup({})
+
+    -- Set up the keymaps after the plugin is loaded
+    local map = vim.keymap.set
+
+    -- Main telescope-like keybindings with leader+f prefix
+    map('n', '<leader>]', '<cmd>BufferLineCycleNext<CR>', { desc = 'BufferLine: Next Buffer' })
+    map('n', '<leader>[', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Bufferline: Prev Buffer' })
   end,
 }
