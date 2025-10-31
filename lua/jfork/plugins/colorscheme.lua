be0d270feb1643e8e235d13f52b277aa97a7655d
jfork <jfork.code@proton.me>
2025-10-31 11:10:30 +0100

    update: change colorscheme to kanagawa

--- lua/jfork/plugins/colorscheme.lua
+++ lua/jfork/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     'ribru17/bamboo.nvim',
     lazy = true,
-    priority = 1000,
+    -- priority = 1000,
     -- config = function()
     --   require('bamboo').setup {}
     --   require('bamboo').load()
@@ -11,19 +11,43 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    lazy = true,
+    -- lazy = false,
+    -- priority = 1000,
     -- config = function()
     --   vim.cmd("colorscheme rose-pine")
     -- end
   },
-  {
-    "rebelot/kanagawa.nvim",
-    name = 'kanagawa',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme kanagawa")
-    end
-  }
+{
+  "rebelot/kanagawa.nvim",
+  priority = 1000,
+  config = function()
+    -- require("kanagawa").setup({
+    --   overrides = function(colors)
+    --     local theme = colors.theme
+    --     return {
+    --       -- Your existing overrides
+    --       NormalFloat = { bg = "none" },
+    --       FloatBorder = { bg = "none" },
+    --       FloatTitle = { bg = "none" },
+    --       NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+    --       LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    --       MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+    --
+    --       -- Add these for noice to use proper kanagawa colors
+    --       NoiceCmdline = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
+    --       NoiceCmdlineIcon = { fg = theme.syn.identifier },
+    --       NoiceCmdlinePopup = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
+    --       NoiceCmdlinePopupBorder = { fg = theme.ui.border },
+    --       NoicePopupmenu = { bg = theme.ui.bg_p1 },
+    --       NoicePopupmenuBorder = { fg = theme.ui.border },
+    --       NoiceConfirm = { bg = theme.ui.bg_p1 },
+    --       NoiceConfirmBorder = { fg = theme.ui.border },
+    --       NoiceMini = { bg = theme.ui.bg_p1 },
+    --     }
+    --   end,
+    -- })
+    vim.cmd("colorscheme kanagawa")
+  end
+}
 
 }
